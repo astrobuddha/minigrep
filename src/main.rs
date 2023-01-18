53a5334ba5301a5cd35544e3ea2c9f6c43620d41
astrobuddha <jozefleo@protonmail.com>
2023-01-17 22:12:18 -0500

    basic checking and getting args from CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    // Prints and returns the value of a given expression for quick and dirty debugging.
+    // dbg!(args);
+
+    if args.len() != 3 {
+        return;
+    }
+
+    let _query = &args[1];
+    let _file_path = &args[2];
+
+    println!("Searching for {}", _query);
+    println!("In file {}", _file_path);
 }
