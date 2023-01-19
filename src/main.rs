4b184d09ba73a1a503c2444167325e0141262d31
astrobuddha <jozefleo@protonmail.com>
2023-01-18 21:46:27 -0500

    currently not compiling, fix todo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 // Prints and returns the value of a given expression for quick and dirty debugging.
 // dbg!(args);
@@ -7,7 +8,10 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
 
 
@@ -15,6 +19,11 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
+    run(config);
+}
+
+
+fn run(config: Config) {
     let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
@@ -27,12 +36,18 @@ struct Config {
 
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+
+        if args.len() != 3 {
+            Return Err(&"incorrect number of arguments") // todo fix this
+        }
+
         let query = &args[1].clone();
         let file_path = &args[2].clone();
-        Config {
+
+        Ok(Config {
             query: query.to_string(),
             file_path: file_path.to_string()
-        }
+        })
     }
 }
