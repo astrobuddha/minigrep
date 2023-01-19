aadf3eb6c794a4ddf4838c40edc317dcdc3d786a
astrobuddha <jozefleo@protonmail.com>
2023-01-18 21:30:36 -0500

    Added lib.rs and refactored to config struct

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,38 @@
 use std::env;
 use std::fs;
 
+// Prints and returns the value of a given expression for quick and dirty debugging.
+// dbg!(args);
+
 fn main() {
-    //println!("Hello, world!");
     let args: Vec<String> = env::args().collect();
 
-    // Prints and returns the value of a given expression for quick and dirty debugging.
-    // dbg!(args);
+    let config = Config::new(&args);
 
-    if args.len() != 3 {
-        return;
-    }
 
-    let _query = &args[1];
-    let _file_path = &args[2];
 
-    println!("Searching for {}", _query);
-    println!("In file {}", _file_path);
 
-    let contents = fs::read_to_string(_file_path).expect("Should have been able to read the file");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String
+}
+
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = &args[1].clone();
+        let file_path = &args[2].clone();
+        Config {
+            query: query.to_string(),
+            file_path: file_path.to_string()
+        }
+    }
 }
