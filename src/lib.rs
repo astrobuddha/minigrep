f9c4881c6235cdbe28893e044a3a1995f8ff737e
astrobuddha <jozefleo@protonmail.com>
2023-01-19 22:22:06 -0500

    fixed up some tests

--- src/lib.rs
+++ src/lib.rs
@@ -26,13 +26,24 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
 }
 
 pub fn search<'a>(_query: &str, _contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+
+    for line in _contents.lines() {
+        if line.contains(_query) {
+            results.push(line);
+        }
+    }
+    
+    results
 }
 
 
@@ -44,10 +55,23 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-            Rust:
-            safe, fast, productive.
-            Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
         
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(vec!["Rust:", "Trust me."], search(query, contents));
     }
 }
