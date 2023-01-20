e4e2f0b0e3d419bc0932594a00d0e0b658d489a4
astrobuddha <jozefleo@protonmail.com>
2023-01-20 06:32:18 -0500

    moved tests into separent file

--- src/lib.rs
+++ src/lib.rs
@@ -45,33 +45,3 @@ pub fn search<'a>(_query: &str, _contents: &'a str) -> Vec<&'a str> {
     
     results
 }
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn one_result() {
-        let query = "duct";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Duct tape.";
-        
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
-    }
-
-    #[test]
-    fn case_insensitive() {
-        let query = "rUsT";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-
-        assert_eq!(vec!["Rust:", "Trust me."], search(query, contents));
-    }
-}
---
+++ tests/lib_tests.rs
@@ -0,0 +1,30 @@
+//use minigrep::lib;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+        
+        assert_eq!(vec!["safe, fast, productive."], minigrep::search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(vec!["Rust:", "Trust me."], minigrep::search(query, contents));
+    }
+}
